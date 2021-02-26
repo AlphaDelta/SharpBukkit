d233d8a1b162817d4744ddb4209ba57dd238a334
AlphaDelta <admin@shaw.co.nz>
2021-02-26 14:11:00 +1300

    Update custom Attribute in accordance with Microsoft standards

--- SharpBukkitLive/SharpBukkit/Interface/Command/SharpBukkitCommandAttribute.cs
+++ SharpBukkitLive/SharpBukkit/Interface/Command/SharpBukkitCommandAttribute.cs
@@ -7,17 +7,14 @@ namespace SharpBukkitLive.Interface.Command
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
     public class SharpBukkitCommandAttribute : Attribute
     {
-        public string Name, Description;
+        public readonly string Name, Description;
         public bool OPOnly, PlayerOnly;
         public bool HideFromSearch;
 
-        public SharpBukkitCommandAttribute(string Description = null, string Name = null, bool OPOnly = false, bool PlayerOnly = false, bool HideFromSearch = false)
+        public SharpBukkitCommandAttribute(string Description = null, string Name = null)
         {
             this.Name = Name;
             this.Description = Description;
-            this.OPOnly = OPOnly;
-            this.PlayerOnly = PlayerOnly;
-            this.HideFromSearch = HideFromSearch;
         }
     }
 }
